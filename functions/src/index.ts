52c02c04935cb80addbfcfb959375eee800c28a5
Erin Morrissey <erinmorrissey@gmail.com>
2018-09-23 00:31:01 -0700

    add getCrystal function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,15 +11,34 @@ export const healthCheck = functions.https.onRequest((request, response) => {
   response.send("Hi, Hello! The Crystals API is healthy.");
 });
 
-export const crystals = functions.https.onRequest((request, response) => {
+// handler for /crystals endpoint
+export const getCrystals = functions.https.onRequest((request, response) => {
   db.collection('crystals').get()
     .then((snapshot) => {
-      console.log('Snapshot: ', snapshot);
       snapshot.forEach((doc) => {
         data.push(doc.data());
         console.log(doc.id, '=>', doc.data());
       });
       response.send(data);
+    })
+    .catch((err) => {
+      console.log('Error getting Firestore data', err);
+    });
+});
+
+// handler for /crystals/<crystal> endpoint
+export const getCrystal = functions.https.onRequest((request, response) => {
+  // splitting & popping because request.url is '/pyrite' when running locally,
+  // but is '/crystals/pyrite' in production b/c of my hosting/url config
+  const crystal = request.url.split('/').pop();
+
+  db.collection('crystals').doc(crystal).get()
+    .then((doc) => {
+      if (!doc.exists) {
+        response.send("We don't seem to have data on that crystal");
+      } else {
+        response.send(doc.data());
+      }
     })
     .catch((err) => {
       console.log('Error getting Firestore data', err);
